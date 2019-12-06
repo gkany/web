a4c8c2e68d701191ef17c271e0738adfa0cc8d95
xukang <xukang@chukong-inc.com>
2019-12-06 13:36:00 +0800

    update

--- flask_web/app/flask_web.py
+++ flask_web/app/flask_web.py
@@ -1,14 +1,19 @@
 from flask import Flask
 import os
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    hostname = os.environ['HOST_OPTS']
-    return '[{}] hello flask web\n'.format(hostname)
+    hostname = 'localhost-test'
+    if 'HOST_OPTS' in os.environ:
+        hostname = os.environ['HOST_OPTS']
+    now_time = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    ret = '[{}][{}] hello flask web\n'.format(hostname, now_time)
+    return ret
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=9999)
+    app.run(host="0.0.0.0", debug=True, port=8041)
 
 
--- flask_web/run-docker.sh
+++ flask_web/run-docker.sh
@@ -4,5 +4,5 @@ name=$2
 tag="flask_web:v"${version}
 
 docker build -t  ${tag}  .
-docker run --name  ${name} -e HOST_OPTS=`hostname`  -dit  -p 9999:9999   ${tag}
+docker run --name  ${name} -e HOST_OPTS=`hostname`  -dit  -p 8041:8041   ${tag}
 
